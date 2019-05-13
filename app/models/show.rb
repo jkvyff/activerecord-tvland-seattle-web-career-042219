39d933f019e8e42c65c0d75866a18a6a463ba90e
github username <github email address>
2019-05-13 08:56:12 -0700

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,8 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_one :network
+
+  def build_network(call_letters)
+  	Network.find_or_create_by(call_letters).shows << self
+  end
 end
